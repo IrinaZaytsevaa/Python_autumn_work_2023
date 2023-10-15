3ef1060926038e2cc0635aee585b7e09eea45131
IrinaZaytsevaa <145017902+IrinaZaytsevaa@users.noreply.github.com>
2023-10-15 17:46:40 +0300

    Delete dz6.2.py

--- dz6.2.py
+++
@@ -1,8 +0,0 @@
-books_first = input().replace(' ', '').split(',')
-books_second = input().replace(' ', '').split(',')
-
-my_lis = []
-for book in books_first:
-    if book in books_second:
-        my_lis.append(book)
-print(len(my_lis))
