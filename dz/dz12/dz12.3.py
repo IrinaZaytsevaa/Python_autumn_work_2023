08703240b48e3a0d7a50a7a3e5bb5d8d905ff74d
IrinaZaytseva <zaitsevaaaa@mail.ru>
2023-10-16 14:24:17 +0300

    dz12

--- dz/dz12/dz12.3.py
+++ dz/dz12/dz12.3.py
@@ -1,28 +1,34 @@
-# s = '1-2, 4-4, 3-6'.split(',')
-# s = ''.join(s).replace('-', '').split()
-# lis, final = [], []
+s = '1-2, 4-4, 3-6, 13-16'
+def ranges(s):
+    ranges_list = s.split(', ')
+    lis = [num for r in ranges_list for num in (range(int(r.split('-')[0]), int(r.split('-')[1])+1) if '-' in r else [int(r)])]
+    return lis
+
+print(ranges(s))
+
+# s = '1-2, 4-4, 3-6, 13-16'
+# def ranges(s):
+#     ranges_list = s.split(', ')
+#     lis = []
+#     for r in ranges_list:
+#         if '-' in r:
+#             start, end = map(int, r.split('-'))
+#             lis.extend(list(range(start, end+1)))
+#         else:
+#             lis.append(int(r))
+#     return lis
 #
-# for i in s:
-#     i = tuple(i)
-#     lis.append(i)
+# print(ranges(s))
+
+
+# import re
+# def ranges(s):
+#     ranges_list = re.findall(r'\d+', s)
+#     lis = []
+#     for i in range(0, len(ranges_list), 2):
+#         lis.append((int(ranges_list[i]), int(ranges_list[i+1])))
+#     return [i for j in lis for i in range(j[0], j[1] + 1)]
 #
-# def ranges(lis):
-#     for j in lis:
-#         for i in range(int(j[0]), int(j[1]) + 1):
-#             final.append(i)
-#     return final
-#
-# print(ranges(lis))
+# input_str = '1-2, 4-4, 3-6, 13-16'
+# print(ranges(input_str))
 
-s = '1-2, 4-4, 3-6'.split(',')
-s = ''.join(s).replace('-', '').split()
-lis = []
-
-for i in s:
-    i = tuple(i)
-    lis.append(i)
-
-def ranges(lis):
-    return [i for j in lis for i in range(int(j[0]), int(j[1]) + 1)]
-
-print(ranges(lis))
