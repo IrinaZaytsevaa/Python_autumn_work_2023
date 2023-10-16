4d6412551059f0f4d805e93fbf0cf4e62e4de71c
IrinaZaytseva <zaitsevaaaa@mail.ru>
2023-10-16 09:31:10 +0300

    dz12

--- dz/dz12/dz12.3.py
+++ dz/dz12/dz12.3.py
@@ -1,3 +1,19 @@
+# s = '1-2, 4-4, 3-6'.split(',')
+# s = ''.join(s).replace('-', '').split()
+# lis, final = [], []
+#
+# for i in s:
+#     i = tuple(i)
+#     lis.append(i)
+#
+# def ranges(lis):
+#     for j in lis:
+#         for i in range(int(j[0]), int(j[1]) + 1):
+#             final.append(i)
+#     return final
+#
+# print(ranges(lis))
+
 s = '1-2, 4-4, 3-6'.split(',')
 s = ''.join(s).replace('-', '').split()
 lis, final = [], []
@@ -7,9 +23,6 @@ for i in s:
     lis.append(i)
 
 def ranges(lis):
-    for j in lis:
-        for i in range(int(j[0]), int(j[1]) + 1):
-            final.append(i)
-    return final
+    return [i for j in lis for i in range(int(j[0]), int(j[1]) + 1)]
 
 print(ranges(lis))
